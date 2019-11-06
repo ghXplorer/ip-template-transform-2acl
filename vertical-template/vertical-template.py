552bd17496f33e72e1df40337c2daab21e1d3c05
ghXplorer <57403493+ghXplorer@users.noreply.github.com>
2019-11-06 13:44:39 +0300

    Update vertical-template.py

--- vertical-template/vertical-template.py
+++ vertical-template/vertical-template.py
@@ -30,53 +30,53 @@ def ip2long(ip):
 
 
 def acl_unfold():
-	new_list = []
-	for i in re.split(r'-+', pyperclip.paste()):
-		new_list.append(i.strip().split())
-	n = 0
-	sorted_list = []
-	for x in new_list[0]:
-		for y in new_list[1]:
-			for z in new_list[2]:
-				print(f'access-list test extended permit tcp host {x} host {y} eq {z}')
-				sorted_list.append(f'access-list test extended permit tcp host {x} host {y} eq {z}')
-				n += 1
-	print('\n', n, sep='')
-	
-	list_of_lists = []
-	for line in sorted_list:
-		list_of_lists.append(line.split())
+    new_list = []
+    for i in re.split(r'-+', pyperclip.paste()):
+        new_list.append(i.strip().split())
+    n = 0
+    sorted_list = []
+    for x in new_list[0]:
+        for y in new_list[1]:
+            for z in new_list[2]:
+                print(f'access-list test extended permit tcp host {x} host {y} eq {z}')
+                sorted_list.append(f'access-list test extended permit tcp host {x} host {y} eq {z}')
+                n += 1
+    print('\n', n, sep='')
+    
+    list_of_lists = []
+    for line in sorted_list:
+        list_of_lists.append(line.split())
 
-	for i in sorted(list_of_lists, key=lambda x: ip2long(x[6])): # sorting IPs by: index 6 for src, 8 for dst
-	    print(' '.join(i))
+    for i in sorted(list_of_lists, key=lambda x: ip2long(x[6])): # sorting IPs by: index 6 for src, 8 for dst
+        print(' '.join(i))
 
 
 def acl_obj_groups():
-	new_list = []
-	for i in re.split(r'-+', pyperclip.paste()):
-		new_list.append(i.strip().split())
-	
-	print()
-	src = f'object-group network SRC-SRV-{current_date}'
-	print(src)
-	for x in new_list[0]:
-		print(f'network-object host {x}')
+    new_list = []
+    for i in re.split(r'-+', pyperclip.paste()):
+        new_list.append(i.strip().split())
+    
+    print()
+    src = f'object-group network SRC-SRV-{current_date}'
+    print(src)
+    for x in new_list[0]:
+        print(f'network-object host {x}')
 
-	print()
-	dst = f'object-group network DST-SRV-{current_date}'
-	print(dst)
-	for x in new_list[1]:
-		print(f'network-object host {x}')
+    print()
+    dst = f'object-group network DST-SRV-{current_date}'
+    print(dst)
+    for x in new_list[1]:
+        print(f'network-object host {x}')
 
-	print()
-	ports = f'object-group service TCP-{current_date} tcp'
-	print(ports)
-	for x in new_list[2]:
-		print(f'port-object eq {x}')
+    print()
+    ports = f'object-group service TCP-{current_date} tcp'
+    print(ports)
+    for x in new_list[2]:
+        print(f'port-object eq {x}')
 
-	print()
-	print(f'access-list test extended permit tcp object-group SRC-SRV-{current_date}'
-		  f' object-group DST-SRV-{current_date} object-group TCP-{current_date}')
+    print()
+    print(f'access-list test extended permit tcp object-group SRC-SRV-{current_date}'
+          f' object-group DST-SRV-{current_date} object-group TCP-{current_date}')
 
 
 choice = input('\n1 for standard_acl or 2 for obj_groups: ')
